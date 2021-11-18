c0ce8e17ba3c6444bb41b0915d87b07ac0a9e7bd
Samantha J <sam.jver@gmail.com>
2021-11-18 12:11:06 -0800

    Lots of snow now falls

--- snowflakes_falling.py
+++ snowflakes_falling.py
@@ -37,13 +37,26 @@ class Snowflake:
             random.randrange(0, SCREEN_WIDTH),
             random.randrange(0, SCREEN_HEIGHT)
         ]
+
+        # self.x_vel = random.randrange(-1, 1)
         self.y_vel = 2
         self.colour = WHITE
 
     def update(self):
         """Update the location of the snow"""
 
-        self.coords[1] += self.y_vel
+        # self.coords[1] += self.y_vel
+        self.coords[0] += self.x_vel
+
+        # reset position of snowflake if it reaches bottom
+        if self.coords[1] == SCREEN_HEIGHT:
+            self.coords = [
+                random.randrange(0, SCREEN_WIDTH),
+                random.randrange(-15, 0)
+            ]
+        # TODO: IF self.coords[0] == SCREEN_WIDTH
+        else:
+            pass
 
 def main() -> None:
     """Driver of the Python script"""
@@ -54,9 +67,21 @@ def main() -> None:
     # Create some local variables that describe the environment
     done = False
     clock = pygame.time.Clock()
+    num_snowflakes = 250
+    snowflakes = []
 
-    # Create a snowflake
-    snow = Snowflake()
+    # Create snowflakes in foreground
+    snowflakes = []
+    for i in range(100):
+        close_snowflake = Snowflake()
+        close_snowflake.size = 3
+        close_snowflake.y_vel = 3
+        snowflakes.append(close_snowflake)
+
+    # Create snowflakes in background
+
+    for i in range(num_snowflakes):
+        snowflakes.append(Snowflake())
 
     # ----------- MAIN LOOP
     while not done:
@@ -67,12 +92,16 @@ def main() -> None:
 
         # ----------- CHANGE ENVIRONMENT
 
-        snow.update()
+        for snow in snowflakes:
+            snow.update()
 
         # ----------- DRAW THE ENVIRONMENT
         screen.fill(BGCOLOUR)      # fill with bgcolor
 
-        pygame.draw.circle(screen, snow.colour, snow.coords, snow.size)
+        # draw the snowflakes
+
+        for snow in snowflakes:
+            pygame.draw.circle(screen, snow.colour, snow.coords, snow.size)
 
         # Update the screen
         pygame.display.flip()
